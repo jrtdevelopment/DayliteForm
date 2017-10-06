1ec521d14f875d46d921afb03ccb8823fa38c486
schwassan <32567148+schwassan@users.noreply.github.com>
2017-10-06 11:07:43 +0100

    First Commit

---
+++ mastercode.py
@@ -0,0 +1,24 @@
+import pprint #Imports for pretty print
+import requests #Imports for http requests to handle the API
+import json #Imports the JSON handler
+apiKey = 'Bearer VNAl8xf8bFiPTSECiA8K17QPN1FT4r' #Put the API key here, saves you having to copy and past it into your code
+
+r = requests.get('https://api.marketcircle.net/v1', headers={'Authorization': apiKey}, verify=False) #Gives the r variable the value of requests get
+
+print r.status_code #Print the value of the status code of r
+print r.headers['content-type'] #print the value of the headers of the request
+  
+print "Welcome to the Daylite API test service"
+print "Hook['params'] is populated with the following request parameters"
+pprint.pprint(Hook['params'])
+firstName = Hook['params']['firstname']
+lastName = Hook['params']['lastname']
+print firstName
+print lastName
+payload = {'first_name' : firstName, 'last_name' : lastName}
+print payload
+r = requests.get('https://api.marketcircle.net/v1/contacts', headers={'Authorization': apiKey}, verify=False)
+print r.encoding
+print r.content
+#r = requests.post('https://api.marketcircle.net/v1/contacts', headers={'Authorization': apiKey}, json=payload)
+#print r.status_code
