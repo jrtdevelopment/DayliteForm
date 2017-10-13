a54c520fdae0176327640ba4a43925db3975808b
schwassan <32567148+schwassan@users.noreply.github.com>
2017-10-13 13:53:51 +0100

    Copy and past from previous.

--- mastercode.py
+++ mastercode.py
@@ -2,14 +2,13 @@ import pprint #Imports for pretty print
 import requests #Imports for http requests to handle the API
 import json #Imports the JSON handler
 
-apiKey = 'Bearer kVitMPD48pE5yUqaIhqMXjgQkL4Wtg' #Put the API key here, saves you having to copy and past it into your code
+apiKey = 'Bearer nR377HuRfL8WM33yQZsJ76t7RpUKkj' #Put the API key here, saves you having to copy and past it into your code
 
 r = requests.get('https://api.marketcircle.net/v1', headers={'Authorization': apiKey}, verify=False) #Gives the r variable the value of requests get
 
 print r.status_code #Print the value of the status code of r
 print r.headers['content-type'] #print the value of the headers of the request
   
-print "Weclome to the the TakeInput Branch"
 print "Welcome to the Daylite API test service"
 print "Hook['params'] is populated with the following request parameters"
 pprint.pprint(Hook['params'])
@@ -19,8 +18,11 @@ email = Hook['params']['email']
 print firstName
 print lastName
 print email
-r = requests.get('https://api.marketcircle.net/v1/contacts', headers={'Authorization': apiKey}, verify=False)
-print r.encoding
-print r.content
-#r = requests.post('https://api.marketcircle.net/v1/contacts', headers={'Authorization': apiKey}, json=payload)
-#print r.status_code
+#r = requests.get('https://api.marketcircle.net/v1/contacts', headers={'Authorization': apiKey}, verify=False)
+#print r.encoding
+#print r.content
+payload = {'first_name':firstName, 'last_name':lastName}
+
+payload = json.dumps(payload)
+r = requests.post('https://api.marketcircle.net/v1/contacts', headers={'Authorization': apiKey}, data=payload, verify=False)
+print r.status_code
